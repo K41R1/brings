d1fb85700fe1f94227381ff80acbf36ba0d7436b
KhairiAbidi <abidikhairi32@gmail.com>
2019-04-25 17:20:51 +0100

    add build script

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ const {hubblePicture, downloadPicture} = require('./actions/hubblePicture');
 
 program
     .version('0.1.0')
-    .option('-a, --interactive', 'Interactive Mode')
     .option('-p, --potd', 'Picture of The Day from Nasa')
     .option('--hubble', 'Random Picture from Hubble Api')
     .parse(process.argv);
 
-hubblePicture().then(downloadPicture);
+if (program.potd) {
+    pictureOfTheDay().then(downloader)
+}
+if (program.hubble) {
+    hubblePicture().then(downloadPicture)
+}
